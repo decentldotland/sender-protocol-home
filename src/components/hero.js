ac12817533264045e45ef0ab8e9a7c2ae0405594
xylophonez <xylophonezygote@gmail.com>
2022-11-07 08:58:58 +0000

    fix: do not hide hero subheader on mobile

--- src/components/hero.js
+++ src/components/hero.js
@@ -15,7 +15,7 @@ function Hero() {
       <div class="bg-gray-100">
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
 
-          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
             <span class="text-black">{heroCopy.title}</span>
           </h1>
           <p class="mb-6 text-lg font-thin text-black lg:text-xl sm:px-16 xl:px-48 dark:text-gray-100">
