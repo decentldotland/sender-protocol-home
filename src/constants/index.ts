8da7d5b7eb8556e1dfb7f8caaf179d86274b50b8
Max Knivets <knivets.m@gmail.com>
2023-02-13 12:33:21 -0500

    chore: change contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 // API
 export const EXM_READ_URL = 'https://api.exm.dev/read/';
 
-export const EXM_WHITELIST_CONTRACT = '025VpFC0ur9O8_05WtmcEHrssCWrWDJU9ZFKw-21Vdk'
+export const EXM_WHITELIST_CONTRACT = 'ad3ArHtdxGur4wis1Zf1nEoQCtQ3KcIPwcDbEmSofQg'
